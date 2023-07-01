c5d3a5c63687822452599fcf3b9526b61db918be
Anna Książek <aksiazek@softak.eu>
2023-07-01 20:47:40 +0200

    add initial, bad state of classes which will illustrate good practices

---
+++ src/main/java/pl/sdacademy/practice/MyItem.java
@@ -0,0 +1,24 @@
+package pl.sdacademy.practice;
+
+public class MyItem {
+
+    private String a;
+    private String t;
+
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public String getT() {
+        return t;
+    }
+
+    public void setT(String t) {
+        this.t = t;
+    }
+}

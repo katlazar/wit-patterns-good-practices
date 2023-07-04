09f0e297772f96f923252149a1059369b39d510e
Anna Książek <aksiazek@softak.eu>
2023-07-04 17:45:25 +0200

    clean names and create play list logic

---
+++ src/main/java/pl/sdacademy/practice/MyPlayList.java
@@ -0,0 +1,36 @@
+package pl.sdacademy.practice;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class MyPlayList {
+
+    public static void main(String[] args) {
+        Track[] items = new Track[10];
+        items[0] = createTrack("Wednesday", "Paint It Black");
+        items[1] = createTrack("Lindsey Stirling", "Underground");
+        items[2] = createTrack("Einaudi", "Expecience");
+        items[3] = createTrack( "Ramin Djawadi", "The Night King");
+        items[4] = createTrack( "The Landing", "Justin Hurtwitz");
+        items[5] = createTrack( "John Williams", "The Imperial March");
+        items[6] = createTrack( "John Williams", "Duel of the Fates");
+
+        for (int i = 0; i < items.length; i++) {
+            System.out.println(items[i].getArtist()+" "+items[i].getTitle());
+        }
+
+        Arrays.sort(items, Comparator.nullsLast(Comparator.comparing(Track::getArtist)));
+
+        for (int i = 0; i < items.length; i++) {
+            System.out.println(items[i].getArtist()+" "+items[i].getTitle());
+        }
+
+    }
+
+    private static Track createTrack( String artist, String title) {
+        Track f = new Track();
+        f.setArtist(artist);
+        f.setTitle(title);
+        return f;
+    }
+}
---
+++ src/main/java/pl/sdacademy/practice/Track.java
@@ -0,0 +1,24 @@
+package pl.sdacademy.practice;
+
+public class Track {
+
+    private String artist;
+    private String title;
+
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}

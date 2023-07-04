713aef3ba3b61d611bfef7fc7ab2fcd7f5fa24e4
Anna Książek <aksiazek@softak.eu>
2023-07-04 18:52:22 +0200

    add builder and start using toString

--- src/main/java/pl/sdacademy/practice/MyPlayList.java
+++ src/main/java/pl/sdacademy/practice/MyPlayList.java
@@ -9,28 +9,28 @@ public class MyPlayList {
         items.add(createTrack("Wednesday", "Paint It Black"));
         items.add(createTrack("Lindsey Stirling", "Underground"));
         items.add(createTrack("Einaudi", "Expecience"));
-        items.add(createTrack( "Ramin Djawadi", "The Night King"));
-        items.add(createTrack( "The Landing", "Justin Hurtwitz"));
-        items.add(createTrack( "John Williams", "The Imperial March"));
-        items.add(createTrack( "John Williams", "Duel of the Fates"));
-        items.add(createTrack( "John Williams", "Battle of the Heroes"));
+        items.add(createTrack("Ramin Djawadi", "The Night King"));
+        items.add(createTrack("The Landing", "Justin Hurtwitz"));
+        items.add(createTrack("John Williams", "The Imperial March"));
+        items.add(createTrack("John Williams", "Duel of the Fates"));
+        items.add(createTrack("John Williams", "Battle of the Heroes"));
 
-        for (Track track:  items) {
-            System.out.println(track.getArtist()+" "+track.getTitle());
+        for (Track track : items) {
+            System.out.println(track);
         }
 
         Collections.sort(items, Comparator.nullsLast(Comparator.comparing(Track::getArtist)));
 
-        for (Track track:  items) {
-            System.out.println(track.getArtist()+" "+track.getTitle());
+        for (Track track : items) {
+            System.out.println(track.getArtist() + " " + track.getTitle());
         }
 
     }
 
-    private static Track createTrack( String artist, String title) {
-        Track f = new Track();
-        f.setArtist(artist);
-        f.setTitle(title);
-        return f;
+    private static Track createTrack(String artist, String title) {
+        return Track.builder()
+                .artist(artist)
+                .title(title)
+                .build();
     }
 }
--- src/main/java/pl/sdacademy/practice/Track.java
+++ src/main/java/pl/sdacademy/practice/Track.java
@@ -1,24 +1,13 @@
 package pl.sdacademy.practice;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class Track {
 
     private String artist;
     private String title;
 
-
-    public String getArtist() {
-        return artist;
-    }
-
-    public void setArtist(String artist) {
-        this.artist = artist;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 }
